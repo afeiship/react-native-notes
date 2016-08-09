915cc51e851933734700b1b16e4e297b41bed705
feizheng <1290657123@qq.com>
2016-08-09 13:58:17 +0800

    initial

---
+++ 004-font-icons/FontIconsExample/_app/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+import styles from './styles/app';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
---
+++ 004-font-icons/FontIconsExample/index.ios.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import App from './_app/App';
+
+
+AppRegistry.registerComponent('FontIconsExample', () => App);
